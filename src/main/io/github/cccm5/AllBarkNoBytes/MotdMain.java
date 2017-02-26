4dd9c010d8fdf7897643196ee3e18f0e94433a49
cccm5 <danieldavidson@aol.com>
2017-02-25 21:07:49 -0500

    First push

---
+++ src/main/io/github/cccm5/AllBarkNoBytes/MotdMain.java
@@ -0,0 +1,45 @@
+package io.github.cccm5.AllBarkNoBytes;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.BlockState;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.material.Tree;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TimeZone;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MotdMain extends JavaPlugin implements Listener{
+    private FileConfiguration config;
+    private Logger logger;
+    private final String TAG = ChatColor.RED +  "[" + ChatColor.DARK_RED + "AllBarkNoBytes" + ChatColor.RED + "] " + ChatColor.RESET ;
+
+    @Override
+    public void onEnable() {
+        logger = this.getLogger();
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+    @EventHandler
+    public void onPlayerRightClick(PlayerInteractEvent e) {
+        if(e.getAction()!= Action.RIGHT_CLICK_BLOCK && (e.getClickedBlock().getType()!= Material.LOG || e.getClickedBlock().getType()!= Material.LOG_2))
+            return;
+        BlockState state = e.getClickedBlock().getState();
+        state.setData(new Tree(((Tree)state.getData()).getSpecies(), BlockFace.SELF));
+        state.update();
+        e.getPlayer().sendMessage(TAG + "Block succesfully changed!");
+    }
+
+}

6a2905462cc3ab4197239ce68cec7b97b6e32ac0
cccm5 <danieldavidson@aol.com>
2017-02-26 11:09:36 -0500

    Add crafting recipes

--- src/main/io/github/cccm5/AllBarkNoBytes/BarkMain.java
+++ src/main/io/github/cccm5/AllBarkNoBytes/BarkMain.java
@@ -2,8 +2,11 @@ package io.github.cccm5.AllBarkNoBytes;
 
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
+
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.TreeSpecies;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.BlockState;
 import org.bukkit.command.Command;
@@ -12,27 +15,46 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.material.MaterialData;
+import org.bukkit.material.TexturedMaterial;
 import org.bukkit.material.Tree;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
 
+
 public class BarkMain extends JavaPlugin implements Listener{
     private Logger logger;
-    private final String TAG = ChatColor.RED +  "[" + ChatColor.DARK_RED + "AllBarkNoBytes" + ChatColor.RED + "] " + ChatColor.RESET ;
+    private final String TAG = ChatColor.RED +  "[" + ChatColor.DARK_RED + "AllBarkNoBytes" + ChatColor.RED + "] " + ChatColor.RESET,
+                         ITEMTAG = "" + ChatColor.BLACK+ ChatColor.AQUA+ChatColor.RED+ChatColor.RESET;
     private ItemStack wandItem;
 
     @Override
     public void onEnable() {
         logger = this.getLogger();
         getServer().getPluginManager().registerEvents(this, this);
-        wandItem= new ItemStack(Material.GOLD_HOE);
-        ItemMeta meta = wandItem.getItemMeta();
-        meta.setDisplayName(ChatColor.RED + "Bark wand");
-        wandItem.setItemMeta(meta);
+        {
+            wandItem = new ItemStack(Material.GOLD_HOE);
+            ItemMeta meta = wandItem.getItemMeta();
+            meta.setDisplayName(ITEMTAG + ChatColor.RED + "Bark wand");
+            wandItem.setItemMeta(meta);
+        }
+        registerRecipe(slabGenerator(Material.STEP,"Smooth Stone Slab",Material.STONE));
+        registerRecipe(slabGenerator(Material.STEP,"Smooth Sandstone Slab",Material.SANDSTONE));
+        registerRecipe(slabGenerator(Material.STEP,"Smooth Quartz Slab",Material.QUARTZ_BLOCK));
+
+        for(TreeSpecies species : TreeSpecies.values()) {
+            ItemStack item = new Tree(species).toItemStack();
+            ItemMeta meta = item.getItemMeta();
+            meta.setDisplayName("Smooth" + meta.getDisplayName());
+            item.setItemMeta(meta);
+            registerRecipe(item);
+        }
     }
     @EventHandler
     @SuppressWarnings("deprecation")
@@ -74,4 +96,50 @@ public class BarkMain extends JavaPlugin implements Listener{
         player.getInventory().addItem(wandItem);
         return true;
     }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent e) {
+        if(!e.getItemInHand().getItemMeta().getDisplayName().startsWith(ITEMTAG))
+            return;
+        switch(e.getBlock().getType()) {
+            case LOG:
+            case LOG_2: {
+                BlockState state = e.getBlock().getState();
+                state.setData(new Tree(((Tree) state.getData()).getSpecies(), BlockFace.SELF));
+                state.update();
+                return;
+            }
+            case STEP:
+            case STONE_SLAB2: {
+                BlockState state = e.getBlock().getState();
+                //state.setData(new Tree(((Tree) state.getData()).getSpecies(), BlockFace.SELF));
+                state.update();
+                return;
+            }
+        }
+    }
+
+    private void registerRecipe(ItemStack item){
+        ItemStack result = item.clone();
+        ItemMeta meta = result.getItemMeta();
+        meta.setDisplayName(ITEMTAG + meta.getDisplayName());
+        result.setItemMeta(meta);
+        ShapedRecipe customRecipe = new ShapedRecipe(result);
+        customRecipe.shape("II","II");
+        customRecipe.setIngredient('I',item.getData());
+        Bukkit.addRecipe(customRecipe);
+    }
+
+    private ItemStack slabGenerator(Material type, String name, Material base){
+        if(type!=Material.STEP || type!=Material.STONE_SLAB2 || type!=Material.WOOD_STEP)
+            throw new IllegalArgumentException("Material type must be step, stone_slab_2 or wood_step");
+        ItemStack item = new ItemStack(type);
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(name);
+        item.setItemMeta(meta);
+        TexturedMaterial data = (TexturedMaterial) item.getData();
+        data.setMaterial(base);
+        return item;
+    }
+
 }
